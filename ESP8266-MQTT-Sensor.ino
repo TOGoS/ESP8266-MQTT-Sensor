64747921491cba252593026026ff2a3361d472d0
TOGoS <togos00@gmail.com>
2018-02-07 00:34:56 -0600

    Fix hexDigit

--- ESP8266-MQTT-Sensor.ino
+++ ESP8266-MQTT-Sensor.ino
@@ -44,7 +44,7 @@ const size_t dhtCount = sizeof(dhts)/sizeof(class DHT);
 char hexDigit(int num) {
   num = num & 0xF;
   if( num < 10 ) return '0'+num;
-  if( num < 16 ) return 'A'+num;
+  if( num < 16 ) return 'A'+(num-10);
   return '?'; // Should be unpossible
 }
 
